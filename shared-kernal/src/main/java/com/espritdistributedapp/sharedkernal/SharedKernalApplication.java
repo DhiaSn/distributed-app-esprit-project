bef61a95682a598d421826671d6621618a0de2f9
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-07-23 14:46:40 +0100

    init shared-kernel

---
+++ shared-kernal/src/main/java/com/espritdistributedapp/sharedkernal/SharedKernalApplication.java
@@ -0,0 +1,13 @@
+package com.espritdistributedapp.sharedkernal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedKernalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharedKernalApplication.class, args);
+    }
+
+}
---
+++ shared-kernal/src/test/java/com/espritdistributedapp/sharedkernal/SharedKernalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espritdistributedapp.sharedkernal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedKernalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
